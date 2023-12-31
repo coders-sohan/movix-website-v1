d654653305a52db49f0ce473ee319280772d966c
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-01 00:22:53 +0600

    Add navigation functionality to "Back to home" button

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 import "./style.scss";
 
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="pageNotFound">
       <ContentWrapper>
         <span className="bigText">404</span>
         <span className="smallText">Page not found!</span>
-        <span className="goHomeText">Back to home</span>
+        <span className="goHomeText" onClick={() => navigate("/")}>
+          Back to home
+        </span>
       </ContentWrapper>
     </div>
   );
