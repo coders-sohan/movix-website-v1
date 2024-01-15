51aa7d802cedfb10d168c6ef82e214a91313ca66
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-15 19:35:00 +0600

    Add movie title and release year to Details page title

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "../../components/AllSections/Details/DetailsBanner";
 import Cast from "../../components/AllSections/Details/Cast";
@@ -6,8 +8,10 @@ import Videos from "../../components/AllSections/Details/Videos";
 import Similar from "../../components/AllSections/Details/Similar";
 import Recommended from "../../components/AllSections/Details/Recommended";
 import "./style.scss";
+import { Helmet } from "react-helmet";
 
 const Details = () => {
+  const [movieTitle, setMovieTitle] = useState("");
   const { mediaType, id } = useParams();
 
   const { data: videosData, loading: videosLoading } = useFetch(
@@ -17,8 +21,30 @@ const Details = () => {
     `/${mediaType}/${id}/credits`
   );
 
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
+  useEffect(() => {
+    let title =
+      data?.title || data?.name || data?.original_title || data?.original_name;
+
+    if (data?.release_date) {
+      title += " (" + dayjs(data?.release_date).format("YYYY") + ")";
+    }
+
+    setMovieTitle(title);
+  }, [data]);
+
+  console.log(movieTitle);
+
   return (
     <div>
+      <Helmet>
+        {loading ? (
+          <title>Loading...</title>
+        ) : (
+          <title>{`${movieTitle} - Movie Details`}</title>
+        )}
+      </Helmet>
       <DetailsBanner
         videosData={videosData?.results}
         videosLoading={videosLoading}
